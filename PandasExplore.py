af2a999e253e5dd6ef3d3fe71cbade71681a2464
Ndumiso Qwele <nduqwele@MacBook-Pro.local>
2022-08-03 11:57:31 +0200

    Added the FPL folder. With latest changes.

---
+++ PandasExplore.py
@@ -0,0 +1,20 @@
+
+
+# we want to generate a team,
+# team size must be 11 players
+# minmax GW:1, DEF = (3,5), MID= (3,5), FWD= (1,3)
+#so our formations are: 
+# DEF 5, MID=3,FWD,=1 
+#DEF 4, MID=3,FWD,=2 , DEF 4 MID 4 FWD 2, DEF 4, MID 5, FWD 1 
+# DEF 3, MID= 4, FWD = 3 - DEF 3 MID 5 FWD 1 
+
+
+
+# I want to be able to genrate an list of players
+
+
+import pandas as pd
+import numpy as np
+
+
+
